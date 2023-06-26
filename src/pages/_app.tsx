4f1372ef47f260607a4d204ddb61b920163256d5
xylaron <aldo6701@gmail.com>
2023-06-27 05:04:01 +0800

    removed black wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,7 @@ import { type AppType } from "next/dist/shared/lib/utils";
 import "styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <div className="h-3/4 w-[412px] overflow-y-auto overflow-x-hidden bg-white shadow-lg">
-        <Component {...pageProps} />
-      </div>
-    </div>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;

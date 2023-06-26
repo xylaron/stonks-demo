6add70e3cc350bf568994ed16c2196f37b67d27b
xylaron <aldo6701@gmail.com>
2023-06-27 01:54:00 +0800

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-import "@/styles/globals.css";
+import "styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <div className="h-3/4 w-[412px] overflow-y-auto overflow-x-hidden bg-white shadow-lg">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 };
 
 export default MyApp;

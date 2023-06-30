642b9028d1c57cdd7cd3dca9beacab290ee7775c
xylaron <aldo6701@gmail.com>
2023-07-01 01:08:57 +0800

    added auth state checking and redirection to login

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
+import { useRouter } from "next/router";
+import firebase_app from "../firebase/config";
+
+const auth = getAuth(firebase_app);
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const router = useRouter();
+  const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (!loading && !user) {
+      void router.push("/login");
+    }
+  }, [user, loading, router]);
+
+  if (loading) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <h1 className="text-5xl font-extrabold">
+            <span>Stonks</span> Demo
+          </h1>
+        </div>
+      </main>
+    );
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
